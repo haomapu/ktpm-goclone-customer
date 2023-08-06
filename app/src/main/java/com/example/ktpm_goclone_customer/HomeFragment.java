030839ed5d80b05ab3bee23941c9fdbe20273378
doducduy20127477 <90464550+doducduy20127477@users.noreply.github.com>
2023-08-06 21:17:57 +0700

    [Homepage] Improve UI/UX

--- app/src/main/java/com/example/ktpm_goclone_customer/HomeFragment.java
+++ app/src/main/java/com/example/ktpm_goclone_customer/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nex3z.notificationbadge.NotificationBadge;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +30,11 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     private NotificationBadge shopping_badge;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewList;
+
+    private RecyclerView.Adapter adapter2;
+    private RecyclerView recyclerViewList2;
 
     private MapsFragment mapsFragment;
 
@@ -73,20 +82,47 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        if (mapsFragment == null){
-            mapsFragment = new MapsFragment();
-        }
-        getChildFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, mapsFragment)
-                .commit();
+//        if (mapsFragment == null){
+//            mapsFragment = new MapsFragment();
+//        }
+//        getChildFragmentManager().beginTransaction()
+//                .replace(R.id.fragment_container, mapsFragment)
+//                .commit();
 
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        shopping_badge = view.findViewById(R.id.shopping_badge);
-        shopping_badge.setNumber(3);
+//        shopping_badge = view.findViewById(R.id.shopping_badge);
+//        shopping_badge.setNumber(3);
+
+        // This adapter for LOCATIONS
+        recyclerViewList = view.findViewById(R.id.view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewList.setLayoutManager(linearLayoutManager);
+        ArrayList<ListLocation> locations = new ArrayList<>();
+        locations.add(new ListLocation("29 Nguyen Huy Luong St.", "29 Nguyễn Huy Lượng, P.14, Q.Bình Thạnh, Hồ Chí Minh"));
+        locations.add(new ListLocation("62 An Diem St.", "62 An Điểm, P.10, Q.5, Hồ Chí Minh, 7000, Vietnam"));
+        locations.add(new ListLocation("63 Mac Dinh Chi St.", "63 Mạc Đỉnh Chi, P.Đa Kao, Q.1, Hồ Chí Minh, 7000, Vietnam"));
+
+        adapter = new LocationAdapter(locations);
+        recyclerViewList.setAdapter(adapter);
+
+
+        // This adapter for SAVE PLACES
+        recyclerViewList2 = view.findViewById(R.id.view2);
+        LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewList2.setLayoutManager(linearLayoutManager2);
+        ArrayList<ListLocation> marks = new ArrayList<>();
+        marks.add(new ListLocation("THPT N....", ""));
+        marks.add(new ListLocation("THPT N...", ""));
+        marks.add(new ListLocation("29 Nguy...", ""));
+        marks.add(new ListLocation("Trường ...", ""));
+        marks.add(new ListLocation("Home...", ""));
+
+        adapter2 = new MarkAdapter(marks);
+        recyclerViewList2.setAdapter(adapter2);
     }
 
 
